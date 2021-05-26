7d8c1478caad5d9f8db4271f02135b30d3808ee2
Noor Mansour <noormansour2303@gmail.com>
2021-05-26 21:08:44 +0200

    Added all 5 simmer.plot plots, Modularized the simulation code using reactive()

--- Surgical_Chain/app.R
+++ Surgical_Chain/app.R
@@ -48,8 +48,11 @@ ui <- fluidPage( theme = bs_theme(version = 4,bootswatch = "flatly"),
         
         # Show a plot of the generated distribution
         mainPanel( tabsetPanel(
-            tabPanel("TAB1", plotOutput("usage")),
-            tabPanel("TAB2", plotOutput("util"))
+            tabPanel("Usage", plotOutput("usage")),
+            tabPanel("Utilziation", plotOutput("util")),
+            tabPanel("Activity Time", plotOutput("activ")),
+            tabPanel("Waiting Time", plotOutput("wait")),
+            tabPanel("Flow Time", plotOutput("flow"))
         )
         
         )
@@ -65,68 +68,86 @@ server <- function(input,output) {
     # use the render functions to create objects that can be passes as outputs
     # acess input values with input$num for example 
     
-    output$usage <- renderPlot(
-        { #curly brackets used for code blocks
-            #simulating a simple health center (Surgical chain edition)
-            set.seed(2021)
-            
-            
-            
-            ### SOME CONSTANTS
-            arrival_rate <- function() {  #Arrival rate either static or stochastic
-                if(input$inter_arrival_times_var == TRUE) {
-                    rexp(1,1/input$inter_arrival_times)
-                } else {
-                    input$inter_arrival_times
-                }
+    
+    # Modularize the simulation using reactive()
+    
+    sim <- reactive({
+        set.seed(2021)
+        
+        
+        
+        ### SOME CONSTANTS
+        arrival_rate <- function() {  #Arrival rate either static or stochastic
+            if(input$inter_arrival_times_var == TRUE) {
+                rexp(1,1/input$inter_arrival_times)
+            } else {
+                input$inter_arrival_times
             }
-            ##Capacity
-            cap_oc <- input$cap_oc
-            cap_or <- input$cap_or
-            cap_wd <- input$cap_ward
-            ##Service Times
-            serv_oc <-
-                if(input$service_times_var == TRUE) {
-                    rexp(1,1/input$service_times)
-                } else {
-                    input$service_times
-                }
-            
-            serv_or <- serv_oc
-            serv_wd <- serv_oc
-            
-            ### SIMULATION ENVIROMENT
-            
-            patient <- trajectory("patients path") %>%
-                ## add nurse intake activity
-                seize("outpatient_clinic", amount = 1) %>%
-                timeout(serv_oc) %>%
-                release("outpatient_clinic") %>%
-                
-                ## add consultation activity with doc
-                seize("operating_room",amount = 1) %>%
-                timeout(serv_or) %>%
-                release("operating_room", amount = 1) %>%
-                
-                ##add a planning activity
-                seize("ward") %>%
-                timeout(serv_wd) %>%
-                release("ward")
-            
-            #for replications use: hospital <- lapply(1:100,function(){ INSERT SIMULATION ENVIOREMENT})
-            
-            
-            hospital <- simmer("hospital") %>%
-                add_resource("outpatient_clinic",cap_oc) %>%
-                add_resource("operating_room",cap_or) %>%
-                add_resource("ward", cap_wd) %>%
-                add_generator("patient", patient,arrival_rate) %>%
-                run(until = 480)
-            
-            plot(get_mon_resources(hospital), metric = "usage", items = "queue")
-            
         }
-    )
+        ##Capacity
+        cap_oc <- input$cap_oc
+        cap_or <- input$cap_or
+        cap_wd <- input$cap_ward
+        ##Service Times
+        serv_oc <-
+            if(input$service_times_var == TRUE) {
+                rexp(1,1/input$service_times)
+            } else {
+                input$service_times
+            }
+        
+        serv_or <- serv_oc
+        serv_wd <- serv_oc
+        
+        ### SIMULATION ENVIROMENT
+        
+        patient <- trajectory("patients path") %>%
+            ## add nurse intake activity
+            seize("outpatient_clinic", amount = 1) %>%
+            timeout(serv_oc) %>%
+            release("outpatient_clinic") %>%
+            
+            ## add consultation activity with doc
+            seize("operating_room",amount = 1) %>%
+            timeout(serv_or) %>%
+            release("operating_room", amount = 1) %>%
+            
+            ##add a planning activity
+            seize("ward") %>%
+            timeout(serv_wd) %>%
+            release("ward")
+        
+        #for replications use: hospital <- lapply(1:100,function(){ INSERT SIMULATION ENVIOREMENT})
+        
+        
+        hospital <- simmer("hospital") %>%
+            add_resource("outpatient_clinic",cap_oc) %>%
+            add_resource("operating_room",cap_or) %>%
+            add_resource("ward", cap_wd) %>%
+            add_generator("patient", patient,arrival_rate) %>%
+            run(until = 480)
+        
+    })
+    
+    output$usage <- renderPlot({ 
+            plot(get_mon_resources(sim()), metric = "usage", items = "queue")
+        })
+    
+    output$util <- renderPlot({ 
+        plot(get_mon_resources(sim()), metric = "utilization")
+    })
+    
+    output$activ <- renderPlot({ 
+        plot(get_mon_arrivals(sim()), metric = "activity_time")
+    })
+    
+    output$wait <- renderPlot({ 
+        plot(get_mon_arrivals(sim()), metric = "waiting_time")
+    })
+    
+    output$flow <- renderPlot({ 
+        plot(get_mon_arrivals(sim()), metric = "flow_time")
+    })
     
     
 }
