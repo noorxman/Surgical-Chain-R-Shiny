429e6ac9fea9edd557505998c4dc567599ce7264
Noor Mansour <noormansour2303@gmail.com>
2021-06-05 15:34:28 +0200

    Only one doc can be in OR now with a warning message if slider are not according to rules; first use of shiny widgets

--- oversteer/app.R
+++ oversteer/app.R
@@ -1,6 +1,7 @@
 ##################### OVERSTEER SIMULATION ############################
 
 library(shiny)
+library(shinyWidgets)
 library(simmer)
 library(simmer.plot)
 
@@ -15,25 +16,22 @@ ui <- fluidPage(
         sidebarPanel(
             actionButton("run", "Run the simulation for 100 units "),
             
-            sliderInput("oc_doc1",
-                        "Is doctor 1 you have in the OC ?",
-                        min = 0,
-                        max = 1,
-                        value = 1,
-                        step = 1),
-        sliderInput("oc_doc2",
-                    "Is the doctor 2 you have in the OC ?",
-                    min = 0,
-                    max = 1,
-                    value = 1,
-                    step = 1),
-    sliderInput("oc_doc3",
-                "Is the doctor 3 you have in the OC ?",
-                min = 0,
-                max = 1,
-                value = 1,
-                step = 1),
-),
+            sliderTextInput("doc1",
+                        "Where should doctor 1 be ?",
+                        choices = c("OC", "OR"),
+                        selected = "OR"),
+                        
+            sliderTextInput("doc2",
+                            "Where should doctor 1 be ?",
+                            choices = c("OC", "OR"),
+                            selected = "OC"),
+            
+            sliderTextInput("doc3",
+                            "Where should doctor 1 be ?",
+                            choices = c("OC", "OR"),
+                            selected = "OC"),
+
+            ),
         # Show a plot of the generated distribution
         mainPanel(
            plotOutput("usage")
@@ -42,39 +40,74 @@ ui <- fluidPage(
 )
 
 # Define server logic required to draw outputs
-server <- function(input, output) {
+server <- function(input, output, session) {
     
     #Constants 
     
     num_doc <- 3
-    ### REACTIVE PART ###
-#always call reactive funcitons with reactiveFunctionName()
-    # Reactive change of the state of the system based on input changes
+### REACTIVE PART ###
+
+    #always call reactive funcitons with reactiveFunctionName()
+    #reactive change of the state of the system based on input changes
     #logic of capacity changes 
     
     oc_capacity <- reactive({
-        input$oc_doc1 + 
-        input$oc_doc2 + 
-        input$oc_doc3
+        (input$doc1 == "OC") + 
+        (input$doc2 == "OC") + 
+        (input$doc3 == "OC")
     })
+    
+    # Alert and reset the slider if more than one doc is selected for OR 
+    observe({
+             if(( (input$doc1 == "OR") + (input$doc2 == "OR") + (input$doc3 == "OR")) > 1) {
+                 sendSweetAlert(session,
+                                title = "INVALID INPUT",
+                                text = "Only one doctor can be in the operating 
+                                room at the same time!",
+                                type = "error")
+                 
+                 updateSliderTextInput(
+                     session = session,
+                     inputId = "doc1",
+                     selected = "OC"
+                 )
+                 
+                 updateSliderTextInput(
+                     session = session,
+                     inputId = "doc2",
+                     selected = "OC"
+                 )
+                 
+                 updateSliderTextInput(
+                     session = session,
+                     inputId = "doc3",
+                     selected = "OC"
+                 )
+             }
+        })
+    
     
     or_capacity <- reactive({
-        1 - input$oc_doc1 +
-        1 - input$oc_doc2 +
-        1 - input$oc_doc3
+        (input$doc1 == "OR") + 
+        (input$doc2 == "OR") + 
+        (input$doc3 == "OR")
     })
     
     
-    ### FIXED PART ###
-        #fixed code that is run once to build the simulation
+### FIXED PART ###
+
+        # Initialize the simulation at the beginning
         oversteer <- simmer("oversteer")
         
+        # Define trajectories to follow
         
         normal <- trajectory("normal") %>%
             
             # Set capacity dynamically based on reactive functions 
             set_capacity("oc", function() {oc_capacity()} ) %>% 
             set_capacity("or", function() {or_capacity()} ) %>% 
+            
+            # Follow the surgical chain
             
             seize("oc") %>%
             timeout(10) %>% 
@@ -84,23 +117,27 @@ server <- function(input, output) {
             seize("or") %>% 
             timeout(10) %>%
             release("or") 
+        
+        # Build the environment with resources and generators
     
         oversteer %>%
-            add_resource('oc', capacity = 1) %>%
+            add_resource('oc', capacity = 0) %>%
             add_resource("or", capacity = 0) %>%
             add_generator("patient", normal, function() {10})
         
 
-    ### RUN THE SIMULATION BASED WITHOUT RESET ### 
+### RUN THE SIMULATION WITHOUT RESET ### 
         
-        #important to save the sim enviorement into and reactive sim variable
-        #that is called like a function
+        #important to save the sim enviorement into a reactive sim variable
+        #that is called like a function in the output part
+        
     sim <- eventReactive(input$run, {
         
         oversteer %>% run(until = now(oversteer) + 100)
     })
     
-    ### RENDER THE PLOTS BASED ON THE SIMULATION ###
+### RENDER THE PLOTS BASED ON THE SIMULATION ###
+
     output$usage <- renderPlot({
         plot(get_mon_resources(sim()))
     })
